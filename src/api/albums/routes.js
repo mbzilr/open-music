b7a5ed1c526e87c05b835da1a2b30ceb3a749272
Malik Bazil Rabbani <asterion911ts@gmail.com>
2025-11-15 19:47:17 +0700

    quick bugfix 2

--- src/api/albums/routes.js
+++ src/api/albums/routes.js
@@ -3,12 +3,6 @@ const routes = (handler) => [
     method: 'POST',
     path: '/albums',
     handler: handler.postAlbumHandler,
-<<<<<<< HEAD
-    options: {
-      auth: false,
-    },
-=======
->>>>>>> 0d1f065 (Reworked API for Songs and Albums)
   },
   {
     method: 'GET',
@@ -19,23 +13,11 @@ const routes = (handler) => [
     method: 'PUT',
     path: '/albums/{id}',
     handler: handler.putAlbumByIdHandler,
-<<<<<<< HEAD
-    options: {
-      auth: false,
-    },
-=======
->>>>>>> 0d1f065 (Reworked API for Songs and Albums)
   },
   {
     method: 'DELETE',
     path: '/albums/{id}',
     handler: handler.deleteAlbumByIdHandler,
-<<<<<<< HEAD
-    options: {
-      auth: false,
-    },
-=======
->>>>>>> 0d1f065 (Reworked API for Songs and Albums)
   },
 ];
 
--- src/api/songs/routes.js
+++ src/api/songs/routes.js
@@ -3,23 +3,11 @@ const routes = (handler) => [
     method: 'POST',
     path: '/songs',
     handler: handler.postSongHandler,
-<<<<<<< HEAD
-    options: {
-      auth: false
-    },
-=======
->>>>>>> 0d1f065 (Reworked API for Songs and Albums)
   },
   {
     method: 'GET',
     path: '/songs',
     handler: handler.getSongsHandler,
-<<<<<<< HEAD
-    options: {
-      auth: false
-    },
-=======
->>>>>>> 0d1f065 (Reworked API for Songs and Albums)
   },
   {
     method: 'GET',
@@ -30,23 +18,11 @@ const routes = (handler) => [
     method: 'PUT',
     path: '/songs/{id}',
     handler: handler.putSongByIdHandler,
-<<<<<<< HEAD
-    options: {
-      auth: false
-    },
-=======
->>>>>>> 0d1f065 (Reworked API for Songs and Albums)
   },
   {
     method: 'DELETE',
     path: '/songs/{id}',
     handler: handler.deleteSongByIdHandler,
-<<<<<<< HEAD
-    options: {
-      auth: false
-    },
-=======
->>>>>>> 0d1f065 (Reworked API for Songs and Albums)
   }
 ];
 
